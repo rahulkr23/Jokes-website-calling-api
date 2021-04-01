5f9325a1cfc5834209d901e4184c10db6bb8770d
rahulkr23 <rahulwhy23@gmail.com>
2021-04-01 23:38:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+// Promises
+  const jokes = document.getElementById('joke');
+  const jokeBtn = document.querySelector('#jokeBtn');
+
+    const generateJokes = () => {
+        const setHeader = {
+            headers: {
+                Accept : "application/json"
+            }
+        }
+
+       fetch('https://icanhazdadjoke.com', setHeader)
+     
+       
+
+        .then((res) =>  res.json() )
+
+        .then((data) => {
+
+            jokes.innerHTML = data.joke;
+
+
+        }).catch((error) => {
+
+           console.log(error);
+    })
+}
+
+jokeBtn.addEventListener('click', generateJokes);
+generateJokes();
+
+
+ 
